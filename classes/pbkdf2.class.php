89710678f81fb18bdda36f07a87b81fb2bc2ac4b
Aaron C <git@acwpd.com>
2015-06-05 23:04:05 -0700

    Creating a framework to push everything to database; also modifying the tab-style tha ti was using and tweaking some formats

--- classes/pbkdf2.class.php
+++ classes/pbkdf2.class.php
@@ -17,49 +17,46 @@ define("HASH_ITERATION_INDEX", 1);
 define("HASH_SALT_INDEX", 2);
 define("HASH_PBKDF2_INDEX", 3);
 
-function create_hash($password)
-{
-    // format: algorithm:iterations:salt:hash
-    $salt = base64_encode(mcrypt_create_iv(PBKDF2_SALT_BYTE_SIZE, MCRYPT_DEV_URANDOM));
-    return PBKDF2_HASH_ALGORITHM . ":" . PBKDF2_ITERATIONS . ":" .  $salt . ":" .
-        base64_encode(pbkdf2(
-            PBKDF2_HASH_ALGORITHM,
-            $password,
-            $salt,
-            PBKDF2_ITERATIONS,
-            PBKDF2_HASH_BYTE_SIZE,
-            true
-        ));
+function create_hash($password) {
+	// format: algorithm:iterations:salt:hash
+	$salt = base64_encode(mcrypt_create_iv(PBKDF2_SALT_BYTE_SIZE, MCRYPT_DEV_URANDOM));
+	return PBKDF2_HASH_ALGORITHM . ":" . PBKDF2_ITERATIONS . ":" . $salt . ":" .
+		base64_encode(pbkdf2(
+			PBKDF2_HASH_ALGORITHM,
+			$password,
+			$salt,
+			PBKDF2_ITERATIONS,
+			PBKDF2_HASH_BYTE_SIZE,
+			true
+		));
 }
 
-function validate_password($password, $correct_hash)
-{
-    $params = explode(":", $correct_hash);
-    if(count($params) < HASH_SECTIONS)
-       return false;
-    $pbkdf2 = base64_decode($params[HASH_PBKDF2_INDEX]);
-    return slow_equals(
-        $pbkdf2,
-        pbkdf2(
-            $params[HASH_ALGORITHM_INDEX],
-            $password,
-            $params[HASH_SALT_INDEX],
-            (int)$params[HASH_ITERATION_INDEX],
-            strlen($pbkdf2),
-            true
-        )
-    );
+function validate_password($password, $correct_hash) {
+	$params = explode(":", $correct_hash);
+	if(count($params) < HASH_SECTIONS) {
+		return false;
+	}
+	$pbkdf2 = base64_decode($params[HASH_PBKDF2_INDEX]);
+	return slow_equals(
+		$pbkdf2,
+		pbkdf2(
+			$params[HASH_ALGORITHM_INDEX],
+			$password,
+			$params[HASH_SALT_INDEX],
+			(int)$params[HASH_ITERATION_INDEX],
+			strlen($pbkdf2),
+			true
+		)
+	);
 }
 
 // Compares two strings $a and $b in length-constant time.
-function slow_equals($a, $b)
-{
-    $diff = strlen($a) ^ strlen($b);
-    for($i = 0; $i < strlen($a) && $i < strlen($b); $i++)
-    {
-        $diff |= ord($a[$i]) ^ ord($b[$i]);
-    }
-    return $diff === 0;
+function slow_equals($a, $b) {
+	$diff = strlen($a) ^ strlen($b);
+	for($i = 0; $i < strlen($a) && $i < strlen($b); $i++) {
+		$diff |= ord($a[$i]) ^ ord($b[$i]);
+	}
+	return $diff === 0;
 }
 
 /*
@@ -77,33 +74,35 @@ function slow_equals($a, $b)
  * This implementation of PBKDF2 was originally created by https://defuse.ca
  * With improvements by http://www.variations-of-shadow.com
  */
-function pbkdf2($algorithm, $password, $salt, $count, $key_length, $raw_output = false)
-{
-    $algorithm = strtolower($algorithm);
-    if(!in_array($algorithm, hash_algos(), true))
-        die('PBKDF2 ERROR: Invalid hash algorithm.');
-    if($count <= 0 || $key_length <= 0)
-        die('PBKDF2 ERROR: Invalid parameters.');
-
-    $hash_length = strlen(hash($algorithm, "", true));
-    $block_count = ceil($key_length / $hash_length);
-
-    $output = "";
-    for($i = 1; $i <= $block_count; $i++) {
-        // $i encoded as 4 bytes, big endian.
-        $last = $salt . pack("N", $i);
-        // first iteration
-        $last = $xorsum = hash_hmac($algorithm, $last, $password, true);
-        // perform the other $count - 1 iterations
-        for ($j = 1; $j < $count; $j++) {
-            $xorsum ^= ($last = hash_hmac($algorithm, $last, $password, true));
-        }
-        $output .= $xorsum;
-    }
-
-    if($raw_output)
-        return substr($output, 0, $key_length);
-    else
-        return bin2hex(substr($output, 0, $key_length));
+function pbkdf2($algorithm, $password, $salt, $count, $key_length, $raw_output = false) {
+	$algorithm = strtolower($algorithm);
+	if(!in_array($algorithm, hash_algos(), true)) {
+		die('PBKDF2 ERROR: Invalid hash algorithm.');
+	}
+	if($count <= 0 || $key_length <= 0) {
+		die('PBKDF2 ERROR: Invalid parameters.');
+	}
+	
+	$hash_length = strlen(hash($algorithm, "", true));
+	$block_count = ceil($key_length / $hash_length);
+	
+	$output = "";
+	for($i = 1; $i <= $block_count; $i++) {
+		// $i encoded as 4 bytes, big endian.
+		$last = $salt . pack("N", $i);
+		// first iteration
+		$last = $xorsum = hash_hmac($algorithm, $last, $password, true);
+		// perform the other $count - 1 iterations
+		for ($j = 1; $j < $count; $j++) {
+			$xorsum ^= ($last = hash_hmac($algorithm, $last, $password, true));
+		}
+		$output .= $xorsum;
+	}
+	
+	if($raw_output) {
+	return substr($output, 0, $key_length);
+	} else {
+	return bin2hex(substr($output, 0, $key_length));
+	}
 }
 ?>
