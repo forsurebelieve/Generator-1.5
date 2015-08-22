af893020659367fb94ed0483344d4a4e44ac5a30
Aaron C <git@acwpd.com>
2015-08-22 14:54:19 -0700

    ugh

--- js/ajax.js
+++ js/ajax.js
@@ -18,6 +18,7 @@
 // Put site's address here, in format "http://sub.domain.tld/path/to/root"
 var sRoot = document.location.origin; // Default: document.location.origin
 var siteName = "Futhark Power Generator 1.5.4";
+var loadInProgress = false;
 
 function adjustMyURL(displayURL) {
 	window.history.pushState({site:sRoot}, siteName, "/" + displayURL + "/");
@@ -31,12 +32,13 @@ function loadMyPage(pageName, targetDiv) {
 	loaderDiv.classList.add("loading");
 	ajax = new XMLHttpRequest();
 	ajax.open("GET", sRoot + pageName, true);
+	loadInProgress = true;
 	ajax.onreadystatechange = function() {
 		if (ajax.readyState === 4) {
 			document.getElementById(targetDiv).innerHTML = ajax.responseText;
 			loaderDiv.classList.remove("loading");
 			loaderDiv.classList.add("hidden");
-			return true;
+			loadInProgress = false;
 		}
 	};
 	ajax.send(null);
@@ -108,8 +110,12 @@ function insertSmallImages() {
 
 function reroll() {
 	insertSmallImages();
-	if (loadMyPage("/raw","content")) {
-		document.querySelector(".reroll").onclick = function () {reroll();}
+	while (loadInProgress === true) {
+		setTimeout(function() {
+			if (loadInProgress === false) {
+				document.querySelector(".reroll").onclick = function () {reroll();}
+			}
+		}, 500);
 	}
 }
 
