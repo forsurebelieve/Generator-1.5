6c716c84f8c97b379a343d970c38005cdf901cbb
Aaron C <git@acwpd.com>
2015-08-22 12:19:44 -0700

    moving JS to bottom of page instead of top

--- include/header.php
+++ include/header.php
@@ -5,4 +5,3 @@
 <title><?php echo $title ?></title>
 <link rel="stylesheet" href="<?php echo $site_root ?>/styles/basic.css" type="text/css">
 <link rel="stylesheet" href="<?php echo $site_root ?>/styles/loader.css" type="text/css">
-<script src="<?php echo $site_root ?>/js/ajax.js" language="javascript"></script>
