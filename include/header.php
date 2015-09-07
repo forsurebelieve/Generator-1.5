8acad08a9a3fa9f89f2945047441e99cd21605e2
Aaron C <git@acwpd.com>
2015-09-07 14:38:48 -0700

    Moving all GA code to analytics.js

--- include/header.php
+++ include/header.php
@@ -8,14 +8,3 @@
 <link rel="stylesheet" href="<?php echo $site_root ?>/styles/sharebutton.css" type="text/css">
 <script src="js/ShareButton.js"></script>
 <script src="js/analytics.js"></script>
-<script>
-	var gaCookie = '<?php echo $gaCookie; ?>';
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-	ga('create', gaTag, 'auto');
-	ga('require', 'linkid', {
-		'cookieName': gaCookie,
-		'duration': 5,
-		'levels': 5});
-	ga('send', 'pageview');
-</script>
