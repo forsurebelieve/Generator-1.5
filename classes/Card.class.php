1236391faa473a7428e683ced261770e2cdf8403
Aaron C <git@acwpd.com>
2016-01-01 18:45:39 -0800

    Reverting to imagick

--- classes/Card.class.php
+++ classes/Card.class.php
@@ -3,40 +3,21 @@
 		public function __construct($value, $suit) {
 			$sourceDirectory = '/home/public/img/cards';
 			
-			$valueImage = imagecreatefrompng($sourceDirectory . '/' . $value . '.png');
+			if (TRUE !== class_exists('Imagick')) { 
+        		echo 'Imagick class does not exist.'; 
+    		}
 			
-			$suitImage = imagecreatefrompng($sourceDirectory . '/' . $suit . '.png');
+			$valueImage = new Imagick();
+			$valueImage->readImage($sourceDirectory . '/' . $value . '.png');
 			
-			// Merge the value onto the suit
-            if (imagecopymerge($suitImage, $valueImage,0,0,0,0,67,100,100)) {
-                if(imagetypes() & IMG_GIF) {
-                    header('Content-Type: image/gif');
-                    imagegif($im);
-                } elseif(imagetypes() & IMG_PNG)  {
-                    header('Content-Type: image/png');
-                    imagepng($im);
-                } 
-            
-                imagedestroy($valueImage);
-                imagedestroy($suitImage);
-            
-            } else {
-                imagedestroy($valueImage);
-                imagedestroy($suitImage);
-                
-                $im = imagecreate(67,100);
-                
-                $bg = imagecolorallocate($im, 255, 255, 255);
-                $black = imagecolorallocate($im, 0, 0, 0);
-
-                // prints a black "P" in the top left corner
-                imagechar($im, 1, 0, 0,'!', $black);
-                
-                header('Content-Type: image/gif');
-                imagegif($im);
-                
-                
-            }
+			$suitImage = new Imagick();
+			$suitImage->readImage($sourceDirectory . '/' . $suit . '.png');
+			
+			// Onto the suit, put the value...
+			$valueImage->compositeImage($suitImage, Imagick::COMPOSITE_OVERLAY, 0, 0);
+			
+			header('Content-Type: image/' . $valueImage->getImageFormat());
+			echo $valueImage;
 		}
 	}
 ?>
