01671c59ecfee4adc9804ee10dc89810bb60f264
Aaron C <git@acwpd.com>
2016-05-06 22:12:55 -0700

    Implement GD image - Attempt 1
    Trying to add support for GD as a fallback for IMagick. first attempt.

--- classes/Card.class.php
+++ classes/Card.class.php
@@ -1,23 +1,49 @@
 <?php
 	class Card {
+		
+		private $sourceDirectory = '/home/public/img/cards';
+		
 		public function __construct($value, $suit) {
-			$sourceDirectory = '/home/public/img/cards';
+					
+			if (class_exists('Imagick')) { 
+        		$outputImage = $this->IMagicConstruct($value,$suit);
+				$fmt = 'image/' . $outputImage->getImageFormat();
+    		} elseif (class_exists('gd')) {
+				$outputImage['img'] = $this->GDConstruct($value, $suit);
+				$fmt = $outputImage['fmt'];
+			} else {
+				die('No image libraries installed!');
+			}
 			
-			if (TRUE !== class_exists('Imagick')) { 
-        		echo 'Imagick class does not exist.'; 
-    		}
-			
+			header('Content-Type: ' . $fmt);
+			echo $valueImage;
+		}
+		
+		private function IMagickConstruct($value, $suit) {
 			$valueImage = new Imagick();
-			$valueImage->readImage($sourceDirectory . '/' . $value . '.png');
+			$valueImage->readImage($this->sourceDirectory . '/' . $value . '.png');
 			
 			$suitImage = new Imagick();
-			$suitImage->readImage($sourceDirectory . '/' . $suit . '.png');
+			$suitImage->readImage($this->sourceDirectory . '/' . $suit . '.png');
 			
 			// Onto the suit, put the value...
 			$valueImage->compositeImage($suitImage, Imagick::COMPOSITE_OVERLAY, 0, 0);
 			
-			header('Content-Type: image/' . $valueImage->getImageFormat());
-			echo $valueImage;
+			return $valueImage;
+		}
+		
+		private function GDConstruct($value, $suit) {
+			$valueImage = imagecreatefrompng($this->sourceDirectory . '/' . $suit . '.png');
+			$suitImage = imagecreatefrompng($this-sourceDirectory . '/' . $value . '.png');
+			$imageData = getimagesize($suitImage);
+			imagecopymerge($suitImage, $valueImage, 0, 0, 0, 0, $imageData[0], $imageData[1], 100);
+			
+			$data = [
+				'img' => $suitImage,
+				'fmt' => $imageData['mime']
+			];
+			
+			return $suitImage;
 		}
 	}
 ?>
