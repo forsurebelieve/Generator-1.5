911538108faf362544020a3dffc0df1bb5c62672
Aaron C <git@acwpd.com>
2017-03-25 11:56:47 -0700

    removing unused files

--- include/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-	/* 
-	 * configuration file for things that will be on every page
-	 */
-	 
- ?>
--- include/connections.php
+++
@@ -1,4 +0,0 @@
-<?php 
-	global $serverRoot;
-	require_once($serverRoot . 'config/passwords.php'); // .gitignore'd'
-?>
