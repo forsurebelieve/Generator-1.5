b30e61ca7ee78d96da16f9c6efed8ad51ccd12d1
Aaron C <git@acwpd.com>
2017-03-26 22:37:10 -0700

    update route mapping

--- public/index.php
+++ public/index.php
@@ -12,20 +12,20 @@
 	
 	$router = new AltoRouter();
 	
-	$router->map('GET','/',					'controller_Home',			'home');
-	$router->map('GET','/a/roll',			'controller_Reroll_All',	'reroll');
-	$router->map('GET','/list',				'controller_List',			'list');
-	$router->map('GET','/credits',			'controller_Credits',		'credits');
-	$router->map('GET','/multi/[i:count]',	'controller_Multi',			'multi');
-	$router->map('GET','/load/[:loadid]',	'controller_Load',			'load');
-	$router->map('GET','/a/load/[:loadid]',	'controller_Load_AJAX',		'load_ajax');
-	$router->map('GET','/donate',			'controller_Donate',		'donate');
+	$router->map('GET',	'/',								'controller_Home');
+	$router->map('GET',	'/a/roll',							'controller_Reroll_All');
+	$router->map('GET',	'/list',							'controller_List');
+	$router->map('GET',	'/credits',							'controller_Credits');
+	$router->map('GET',	'/multi/[i:count]',					'controller_Multi');
+	$router->map('GET',	'/load/s/[:loadid]',				'controller_Load_Saved');
+	$router->map('GET',	'/load/[:type]/[:flavor]/[:twist]',	'controller_Load');
+	$router->map('GET',	'/donate',							'controller_Donate');
 
 	$match = $router->match();
 
 	// call closure or throw 404 status
 	if( $match && is_callable( $match['target'] ) ) {
-		call_user_func_array( $match['target'], $match['params'] ); 
+		call_user_func( $match['target'], $match['params'] ); 
 	} else {
 		// no route was matched
 		header( $_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
