9b63f62fc32b4b85040077aeb2a10fab00620f6f
Aaron C <aaron@acwpd.com>
2017-03-28 13:17:24 -0700

    providing a sample config.php file.

---
+++ private/config.sample.php
@@ -0,0 +1,7 @@
+<?php
+	/*
+	 * This file manages configuration options for Firebase, as well as any other things that need global configuration.
+	 */
+	define('FIREBASE_DEFAULT_URL',''); // The URL of your firebase database
+	define('FIREBASE_DEFAULT_TOKEN',''); // Settings -> Service Accounts -> Secrets. Yes, it's deprecated.
+	
