5789e53b3407fbf81f634fd46f1cfd5256325825
Aaron C <aaron@acwpd.com>
2017-03-28 17:01:46 -0700

    Updating CSS, and adding Intercooler

--- private/templates/main.frame.html.php
+++ private/templates/main.frame.html.php
@@ -1,11 +1,24 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 		<title><?php echo 'Futhark Generator ' . APP_VERSION; ?></title>
-		<link rel="stylesheet" href="//futhark.acwpd.com/styles/basic.css" type="text/css">
-		<link rel="stylesheet" href="//futhark.acwpd.com/styles/loader.css" type="text/css">
-		<link rel="stylesheet" href="//futhark.acwpd.com/styles/sharebutton.css" type="text/css">
+
+		<!-- Bootstrap -->
+		<link rel="stylesheet" href="/css/basic.css" type="text/css">
+		<link rel="stylesheet" href="/css/loader.css" type="text/css">
+		<link rel="stylesheet" href="/css/sharebutton.css" type="text/css">
 		<!-- <link rel="stylesheet" href="https://www.sharebutton.co/fonts/v2/entypo.min.css" type="text/css"> -->
 		{{css}}
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
 	</head>
 	<body>
 		<div class='headBar'>
@@ -74,6 +87,9 @@
 	</body>
 	<!-- <script src="/js/ShareButton.js"></script>
 	<script src="/js/analytics.js"></script> -->
-	<script src="/js/ajax.js" language="javascript"></script> -->
-	{{js}}
+	<script src="/js/ajax.js" language="javascript"></script>
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="/js/jquery-3.2.0.js"></script>
+	<script src="/js/jquery-migrate-3.0.0.js"></script>
+    {{js}}
 </html>
