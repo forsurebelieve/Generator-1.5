30e472fc5c2b801e182b678422d68f455603560c
Aaron C <aaron@acwpd.com>
2017-03-31 14:13:53 -0700

    switching the onclick to be handled by intercooler

--- private/templates/home.html.php
+++ private/templates/home.html.php
@@ -1,6 +1,8 @@
-<p>Your Roll: (<a href="#" onclick="reroll();">Roll again?</a>)</p>
+<p>Your Roll: (<a href="#" id="reroll" ic-get-from="/a/roll" ic-target="#content" ic-on-beforeSend="insertSmallImages()">Roll again?</a>)</p>
 <?php
 $power = new \ACWPD\Futhark\Power(['twist' => 'Tarot']);
 
 echo $power->getBigImageTable();
 echo $power->getVerboseDescription();
+
+?>
