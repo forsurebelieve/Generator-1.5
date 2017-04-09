26ff946608babc953c7c49f5f5dd1312f97cae8e
Aaron C <git@acwpd.com>
2017-04-09 15:48:14 -0700

    Yeah, I'm an idiot.
    Removing duplicate classes. "I don't need composer, I can use someone
    else's PSR-4 Autoloader" ... "Yes, but then I have to manually register
    my classes, and..."

--- protected/config.php
+++ protected/config.php
@@ -12,11 +12,9 @@
 	/**************************************************************************
 	* Firebase database config.
 	* Firebase database connection provided in ktamas77/firebase-php
-	**************************************************************************/
-	define("FIREBASE_CONNECTION_FILE",DOCUMENT_ROOT . '/protected/futharkgenerator-firebase-adminsdk-5uyfr-ec35410d9b.json');
-	
+	**************************************************************************/	
 	define("FIREBASE_DEFAULT_URL",'https://futharkgenerator.firebaseio.com');
-	define("FIREBASE_DEFAULT_TOKEN",'II8N7YkggxtWa5l9os6h6TE5VvqtyiBI5qVGR5w3');
+	define("FIREBASE_DEFAULT_TOKEN",'umhZacbSOc7l07AzL2ufQbs8yA65ZUZpv5B7tOhG');
 	define("FIREBASE_DEFAULT_PATH",'/');
 
  ?> 
--- protected/config.sample.php
+++ protected/config.sample.php
@@ -13,8 +13,6 @@
 	* Firebase database config.
 	* Firebase database connection provided in ktamas77/firebase-php
 	**************************************************************************/
-	define("FIREBASE_CONNECTION_FILE",DOCUMENT_ROOT . 'path/to/firebase/secure.json');
-	
 	define("FIREBASE_DEFAULT_URL",'https://projectname.firebaseio.com');
 	define("FIREBASE_DEFAULT_TOKEN",'FirebaseSecretKey');
 	define("FIREBASE_DEFAULT_PATH",'/');
