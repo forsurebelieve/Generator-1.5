6d53e34ee556d8b944f5e8852276e72dec59d5ab
Aaron C <git@acwpd.com>
2017-06-17 16:23:38 -0700

    corrected staging document root

--- public/index.php
+++ public/index.php
@@ -2,9 +2,9 @@
 	define('SITE_ROOT','//' . $_SERVER['SERVER_NAME']);
 	
 	if (isset($_SERVER['NFSN_SITE_ROOT'])) {
-		define('DOCUMENT_ROOT',$_SERVER['NFSN_SITE_ROOT']);
+		define('DOCUMENT_ROOT', $_SERVER['NFSN_SITE_ROOT']);
 	} else {
-		define('DOCUMENT_ROOT',dirname($_SERVER['DOCUMENT_ROOT'] . '/'));
+		define('DOCUMENT_ROOT', dirname($_SERVER['DOCUMENT_ROOT']) . '/');
 	}
 
 	require_once DOCUMENT_ROOT . 'protected/config.php';
