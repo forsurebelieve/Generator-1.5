656d0180914866b281f1115d4a63a1a171fc77c1
Aaron C <aaron@acwpd.com>
2019-04-28 12:56:26 +0200

    Use the example element in addition to the element combination

--- protected/classes/PowerFlavor.php
+++ protected/classes/PowerFlavor.php
@@ -25,7 +25,8 @@ namespace ACWPD\Futhark;
 		}
 
 		public function getVerboseDescription() {
-			$output = '<p>You also rolled the <strong>' . $this->flavor['name'] . '</strong> rune! This results in the flavor <strong>' . $this->flavor['flavor'] . '</strong>, which gives a power that ' . $this->flavor['description'] . '. Optionally, if your power requires an element in order to manifest, the element associated with this rune is <strong>' . $this->flavor['element'] . '</strong></p>' . "\n";
+			$example = $this->flavor['elementname'] . '(' . $this->flavor['element'] . ')';
+			$output = '<p>You also rolled the <strong>' . $this->flavor['name'] . '</strong> rune! This results in the flavor <strong>' . $this->flavor['flavor'] . '</strong>, which gives a power that ' . $this->flavor['description'] . '. Optionally, if your power requires an element in order to manifest, the element associated with this rune is <strong>' . $example . '</strong></p>' . "\n";
 
 			return $output;
 		}
