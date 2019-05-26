99beeacffe401cebd35fe62a7508ca1c2f797447
Aaron C <aaron@acwpd.com>
2019-05-26 16:41:18 +0200

    * SO MUCH WHITE SPACE! * Typos * Organized routes better * Named routes (because I can, and it's a best-practices thing) * Updated templates to use this routing better, and also actually function. * Updated templates to address #32 (form fields)

--- protected/slim-routes.php
+++ protected/slim-routes.php
@@ -1,132 +1,141 @@
 <?php
+use ACWPD\Futhark\Power;
 use Slim\App;
 use Slim\Http\Request;
 use Slim\Http\Response;
-use ACWPD\Power;
 
 return function (App $app) {
-	$container = $app->getContainer();
-	$meta = [
-		'title' => $container->get('settings')['APP_TITLE'] . $container->get('settings')['APP_VERSION'],
-		'version' => $container->get('settings')['APP_VERSION']
-	];
-	/*
-	$app->get('/tickets', function (Request $request, Response $response) {
-		$this->logger->addInfo('Ticket list');
-		$mapper = new TicketMapper($this->db);
-		$tickets = $mapper->getTickets();
+    $container = $app->getContainer();
+    $meta = [
+        'title' => $container->get('settings')['APP_TITLE'] . $container->get('settings')['APP_VERSION'],
+        'version' => $container->get('settings')['APP_VERSION'],
+    ];
 
-		$response = $this->view->render($response, 'tickets.phtml', ['tickets' => $tickets]);
-		return $response;
-	});
+    /*
+    $app->get('/tickets', function (Request $request, Response $response) {
+    $this->logger->addInfo('Ticket list');
+    $mapper = new TicketMapper($this->db);
+    $tickets = $mapper->getTickets();
 
-	$app->post('/ticket/new', function (Request $request, Response $response) {
-		$data = $request->getParsedBody();
-		$ticket_data = [];
-		$ticket_data['title'] = filter_var($data['title'], FILTER_SANITIZE_STRING);
-		$ticket_data['description'] = filter_var($data['description'], FILTER_SANITIZE_STRING);
-	});
-	*/
+    $response = $this->view->render($response, 'tickets.phtml', ['tickets' => $tickets]);
+    return $response;
+    });
 
-	$app->get('/',function (Request $request, Response $response) use ($container, $meta) { 
-		$power = new Power($container);
-		$power->loadPowersDB();
+    $app->post('/ticket/new', function (Request $request, Response $response) {
+    $data = $request->getParsedBody();
+    $ticket_data = [];
+    $ticket_data['title'] = filter_var($data['title'], FILTER_SANITIZE_STRING);
+    $ticket_data['description'] = filter_var($data['description'], FILTER_SANITIZE_STRING);
+    });
+     */
+
+    $app->get('/', function (Request $request, Response $response) use ($container, $meta) {
+        $power = new Power($container);
+        $power->loadPowersDB();
 		$data = $power->withRandomType()
 			->withRandomFlavor()
 			->withRandomTwist()
-			->getPowerData();
-		$args = array_merge($data,$meta);
-		
-		return $container->get('renderer')->render($response, 'FullPageBigImages.phtml', $args);
-	});
+            ->getPowerData();
+        $args = array_merge($data, $meta);
 
-	$app->get('/list',function (Request $request, Response $response) use ($container, $meta) { 
-		$power = new Power($container);
-		$power->loadPowersDB();
-		$data = $power->withAllData()
-			->getPowerData();
-		$args = array_merge($data,$meta);
-		
-		return $container->get('renderer')->render($response, 'List.phtml', $args);
-	});
+        return $container->get('renderer')->render($response, 'FullPageBigImages.phtml', $args);
+    })->setName('home');
 
-	$app->get('/credits',function (Request $request, Response $response) use ($container, $meta) {
-		return $container->get('renderer')->render($response, 'Credits.phtml', $meta);
-	});
+    $app->get('/list', function (Request $request, Response $response) use ($container, $meta) {
+        $power = new Power($container);
+        $power->loadPowersDB();
+        $data = $power->withAllData()
+            ->getPowerData();
+        $args = array_merge($data, $meta);
 
-	$app->get('/donate',function (Request $request, Response $response) use ($container, $meta) {
-		return $container->get('renderer')->render($response, 'Donate.phtml', $meta);
-	});
+        return $container->get('renderer')->render($response, 'List.phtml', $args);
+    })->setName('list');
 
-	$app->group('/a', function (App $app) use ($container) {
-		$app->group('/roll', function (App $app) use ($container) {
-			$app-get('/', function (Request $request, Response $response) use ($container) {
-				$power = new Power($container);
-				$power->loadPowersDB();
-				$data = $power->withRandomType()
-					->withRandomFlavor()
-					->withRandomTwist()
-					->getPowerData();
-				$args = array_merge($data);
-				
-				return $container->get('renderer')->render($response, 'BigImages.phtml', $args);
-			});
+    $app->get('/credits', function (Request $request, Response $response) use ($container, $meta) {
+        return $container->get('renderer')->render($response, 'Credits.phtml', $meta);
+    })->setName('credits');
 
-			$app->get('/type', function (Request $request, Response $response) use ($container) {
-				$power = new Power($container);
-				$power->loadPowersDB();
-				$data = $power->withRandomType()
-					->getPowerData();
-				$args = array_merge($data);
-				
-				return $container->get('renderer')->render($response, 'OnlyType.phtml', $args);
-			});
+    $app->get('/donate', function (Request $request, Response $response) use ($container, $meta) {
+        return $container->get('renderer')->render($response, 'Donate.phtml', $meta);
+    })->setName('donate');
 
-			$app->get('/flavor', function (Request $request, Response $response) use ($container) {
-				$power = new Power($container);
-				$power->loadPowersDB();
-				$data = $power->withRandomFlavor()
-					->getPowerData();
-				$args = array_merge($data);
-				
-				return $container->get('renderer')->render($response, 'OnlyFlavor.phtml', $args);
-			});
+    $app->get('/load/{type_}/{flavor_}/{twist_}', function (Request $request, Response $response, array $args) use ($container) {
+        $power = new Power($container);
+        $power->loadPowersDB();
+        $data = $power->withType($args['type_'])
+            ->withFlavor($args['flavor_'])
+            ->withTwist($args['twist_'])
+            ->getPowerData();
+        $args = array_merge($data);
 
-			$app->get('/twist', function (Request $request, Response $response) use ($container) {
-				$power = new Power($container);
-				$power->loadPowersDB();
-				$data = $power->withRandomTwist()
-					->getPowerData();
-				$args = array_merge($data);
-				
-				return $container->get('renderer')->render($response, 'OnlyTwist.phtml', $args);
-			});
-		});
+        return $container->get('renderer')->render($response, 'FullPageBigImages.phtml', $args);
+    })->setName('load');
 
-		$app->get('/load/{type_}/{flavor_}/{twist_}', function (Request $request, Response $response, array $args) use ($container) {
-			$power = new Power($container);
-			$power->loadPowersDB();
-			$data = $power->withType($args['type_'])
-				->withFlavor($args['flavor_'])
-				->withTwist($args['twist_'])
-				->getPowerData();
-			$args = array_merge($data);
-			
-			return $container->get('renderer')->render($response, 'BigImages.phtml', $args);
-		});
-	});
-	$app->get('/load/{type_}/{flavor_}/{twist_}', function (Request $request, Response $response, array $args) use ($container) {
-		$power = new Power($container);
-		$power->loadPowersDB();
-		$data = $power->withType($args['type_'])
-			->withFlavor($args['flavor_'])
-			->withTwist($args['twist_'])
-			->getPowerData();
-		$args = array_merge($data);
-		
-		return $container->get('renderer')->render($response, 'FullPageBigImages.phtml', $args);
-	});
-	$app->post('/save', function(Request $request, Response $response, array $args) use ($container) {
+    $app->get('/load/{id_}', function (Request $request, Response $response, array $args) use ($container) {
+        $fb = $container->get('firebase')->getDatabase();
+        $ref = $fb->getReference('saved/' . $args['id_']);
+        $data = $ref->getValue();
+    })->setName('loadSaved');
 
-	});
+    $app->post('/save', function (Request $request, Response $response, array $args) use ($container) {
+
+    });
+
+    $app->group('/a', function (App $app) use ($container) {
+        $app->group('/roll', function (App $app) use ($container) {
+            $app->get('/', function (Request $request, Response $response) use ($container) {
+                $power = new Power($container);
+                $power->loadPowersDB();
+                $data = $power->withRandomType()
+                    ->withRandomFlavor()
+                    ->withRandomTwist()
+                    ->getPowerData();
+                $args = array_merge($data);
+
+                return $container->get('renderer')->render($response, 'BigImages.phtml', $args);
+            });
+
+            $app->get('/type', function (Request $request, Response $response) use ($container) {
+                $power = new Power($container);
+                $power->loadPowersDB();
+                $data = $power->withRandomType()
+                    ->getPowerData();
+                $args = array_merge($data);
+
+                return $container->get('renderer')->render($response, 'OnlyType.phtml', $args);
+            });
+
+            $app->get('/flavor', function (Request $request, Response $response) use ($container) {
+                $power = new Power($container);
+                $power->loadPowersDB();
+                $data = $power->withRandomFlavor()
+                    ->getPowerData();
+                $args = array_merge($data);
+
+                return $container->get('renderer')->render($response, 'OnlyFlavor.phtml', $args);
+            });
+
+            $app->get('/twist', function (Request $request, Response $response) use ($container) {
+                $power = new Power($container);
+                $power->loadPowersDB();
+                $data = $power->withRandomTwist()
+                    ->getPowerData();
+                $args = array_merge($data);
+
+                return $container->get('renderer')->render($response, 'OnlyTwist.phtml', $args);
+            });
+        });
+
+        $app->get('/load/{type_}/{flavor_}/{twist_}', function (Request $request, Response $response, array $args) use ($container) {
+            $power = new Power($container);
+            $power->loadPowersDB();
+            $data = $power->withType($args['type_'])
+                ->withFlavor($args['flavor_'])
+                ->withTwist($args['twist_'])
+                ->getPowerData();
+            $args = array_merge($data);
+
+            return $container->get('renderer')->render($response, 'BigImages.phtml', $args);
+        });
+    });
 };
