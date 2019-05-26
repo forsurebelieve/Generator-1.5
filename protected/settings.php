1b51a8142c02393b5d94a3200e02ca2c9ada82bb
Aaron C <aaron@acwpd.com>
2019-05-26 16:28:36 +0200

    Settings and logging cleanup

--- protected/settings.php
+++ protected/settings.php
@@ -3,15 +3,16 @@ return [
     'settings' => [
         'displayErrorDetails' => true, // set to false in production
         'addContentLengthHeader' => false, // Allow the web server to send the content-length header
-        
+
         // Renderer settings
         'renderer' => [
             'template_path' => __DIR__ . '/templates/',
         ],
+        
         // Monolog settings
         'logger' => [
-            'name' => 'slim-app',
-            'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
+            'name' => 'futhark',
+            'path' => __DIR__ . '/../logs/app.log',
             'level' => \Monolog\Logger::DEBUG,
         ],
 
@@ -31,9 +32,7 @@ return [
         ],
 
         'powersDB' => [
-            'location' => __DIR__ . '/sourceData/firebase.english.json'
+            'location' => __DIR__ . '/../sourceData/firebase.english.json'
         ]
-    ],
-
-    
+    ], 
 ];
