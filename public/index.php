d92f98aad62c09fb582cea949fff55be23f9bce5
Aaron C <aaron@acwpd.com>
2019-09-28 16:30:36 +0200

    Can't do that there

--- public/index.php
+++ public/index.php
@@ -16,13 +16,9 @@ require __DIR__ . '/../protected/vendor/autoload.php';
 
 // Load environment variables (if in dev)
 if (!isset($_ENV['environment']) || $_ENV['environment'] == 'dev') {
-    $logger->info('DotEnv set!');
     $dotenv = Dotenv\Dotenv::create(__DIR__ . '/../protected/', '.env');
     $dotenv->load();
-} else {
-    $logger->info('DotEnv not set!');
 }
-
 
 // Instantiate the app
 $settings = require __DIR__ . '/../protected/settings.php';
